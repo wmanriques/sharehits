793e730914c0a13229795a55a7af30ce3499832b
wils0n <wiljm0.0@gmail.com>
2015-07-14 10:59:55 -0500

    update api list rooms only public 2

--- modules/rooms/urls.py
+++ modules/rooms/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from .views import RoomPublicList, RoomDetail, TagList, SongList, User_RoomList
 
 urlpatterns = [
-	url(r'^api/rooms/public$', RoomPublicList.as_view(), name='room-list'),
+	url(r'^api/rooms/public/$', RoomPublicList.as_view(), name='room-list'),
 	url(r'^api/rooms/(?P<pk>[0-9]+)/$', RoomDetail.as_view()),
 	url(r'^api/tags/$', TagList.as_view(), name='tag-list'),
 	url(r'^api/songs/$', SongList.as_view(), name='song-list'),

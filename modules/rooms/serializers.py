d92fe5172b2ce4e444d4333e9a5b0f34ee051230
wils0n <wiljm0.0@gmail.com>
2015-07-15 12:23:49 -0500

    update tag for rooms

--- modules/rooms/serializers.py
+++ modules/rooms/serializers.py
@@ -4,6 +4,11 @@ from .models import Room, Song, Tag, User_Room
 
 from rest_framework import serializers
 
+class TagSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Tag
+		fields = ('id', 'name')
+		
 
 class RoomSerializer(serializers.ModelSerializer):
 	#tag = serializers.StringRelatedField(many=True)
@@ -11,22 +16,13 @@ class RoomSerializer(serializers.ModelSerializer):
         read_only=True,
         slug_field='username'
      )
-	tag = serializers.SlugRelatedField(
-		many=True,
-		read_only=True,
-        slug_field='name'
-     )
+	#tag = serializers.StringRelatedField(many=True)
+	tag = TagSerializer(many=True, read_only=True)
 	#tag = serializers.HyperlinkedIdentityField('tag', view_name='tag-list')
 
 	class Meta:
 		model = Room
 		fields = ('id', 'name', 'image', 'mode', 'creator', 'tag', 'date_creation')
-
-
-class TagSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Tag
-		fields = ('id', 'name')
 
 
 class SongSerializer(serializers.ModelSerializer):

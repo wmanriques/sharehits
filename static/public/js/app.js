35a93340b7718ca09612744e0d0b634f772524d1
wils0n <wiljm0.0@gmail.com>
2015-07-15 16:00:08 -0500

    add my rooms from user authenticated

--- static/public/js/app.js
+++ static/public/js/app.js
@@ -1,25 +1,37 @@
 
-angular.module("sharehits", [
-        "ngMaterial",
-    ])
-	.config(function($interpolateProvider){
-          $interpolateProvider.startSymbol('[[').endSymbol(']]');
-     })
-	.service('RoomFactory', ['$http', function ($http) {
-	    return {
-	        getAll: function () {
-	            return $http.get('http://localhost:8000/api/rooms/public');
-	        }
-	    }
-	}])
+app.service('RoomFactory', ['$http', function ($http) {
+    return {
+        getAll: function () {
+            return $http.get('http://localhost:8000/api/rooms/public');
+        }
+    }
+}]);
 
-    .controller('mainController', function($scope, RoomFactory){
-         RoomFactory.getAll().then(function(res){
-	        $scope.salas = [];
-	        res.data.forEach(function(item){
-	        	console.log(item);
-	        	$scope.salas.push(item);
-	        })
-	        return $scope;
-	    })
-    });
+app.service('RoomUserFactory', ['$http', 'user', function ($http, user) {
+	//user_auth=user.username
+	user_auth="alexander"
+    return {
+        getAll: function () {
+            return $http.get('http://localhost:8000/api/rooms/'+user_auth);
+        }
+    }
+}]);
+
+app.controller('mainController', function($scope, RoomFactory, RoomUserFactory){
+     RoomFactory.getAll().then(function(res){
+        $scope.salas = [];
+        res.data.forEach(function(item){
+        	$scope.salas.push(item);
+        })
+        return $scope;
+    })
+
+    RoomUserFactory.getAll().then(function(res){
+        $scope.mis_salas = [];
+        res.data.forEach(function(item){
+        	console.log(item);
+        	$scope.mis_salas.push(item);
+        })
+        return $scope;
+    })
+});

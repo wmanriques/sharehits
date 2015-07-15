1966621d0835787056176e1ccd36b6a85d5cba66
wils0n <wiljm0.0@gmail.com>
2015-07-15 12:40:54 -0500

    integrate front and back rooms

--- static/public/js/app.js
+++ static/public/js/app.js
@@ -1,7 +1,10 @@
+
 angular.module("sharehits", [
         "ngMaterial",
     ])
-	
+	.config(function($interpolateProvider){
+          $interpolateProvider.startSymbol('[[').endSymbol(']]');
+     })
 	.service('RoomFactory', ['$http', function ($http) {
 	    return {
 	        getAll: function () {
@@ -12,9 +15,9 @@ angular.module("sharehits", [
 
     .controller('mainController', function($scope, RoomFactory){
          RoomFactory.getAll().then(function(res){
-         	
 	        $scope.salas = [];
 	        res.data.forEach(function(item){
+	        	console.log(item);
 	        	$scope.salas.push(item);
 	        })
 	        return $scope;

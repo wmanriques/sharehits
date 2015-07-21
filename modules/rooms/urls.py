60c620c1bc25fb9846678cf75e7ff5d3455cee3d
wils0n <wiljm0.0@gmail.com>
2015-07-21 17:18:45 -0500

    fix url for user in room

--- modules/rooms/urls.py
+++ modules/rooms/urls.py
@@ -5,7 +5,7 @@ from modules.rooms import api
 
 urlpatterns = [
 	url(r'^api/rooms/public/$', RoomPublicList.as_view(), name='api-room-list-public'),
-	url(r'^api/rooms/(?P<username>[0-9a-zA-Z_-]+)/$', RoomUserList.as_view(), name='api-room-list-user'),
+	url(r'^api/(?P<username>[0-9a-zA-Z_-]+)/rooms/$', RoomUserList.as_view(), name='api-room-list-user'),
 	#url(r'^api/room/(?P<pk>[0-9]+)/$', RoomDetail.as_view()),
 	url(r'^api/room/new/', api.room_create, name='api-room-create'),
 	url(r'^api/tags/$', TagList.as_view(), name='api-tag-list'),

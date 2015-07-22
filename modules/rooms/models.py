47723dd0314342f3800a42b3fa6201a9131e0a69
wils0n <wiljm0.0@gmail.com>
2015-07-22 01:19:57 -0500

    add room done

--- modules/rooms/models.py
+++ modules/rooms/models.py
@@ -31,7 +31,7 @@ class Tag(models.Model):
 	name = models.CharField(max_length=50)
 
 	def __unicode__(self):
-		return u"{}".format(self.name)
+		return u"tag : {}".format(self.name)
 
 
 class User_Room(models.Model):

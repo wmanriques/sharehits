847ae0050a1f195c42eee3dc79bade010adf7014
wils0n <wiljm0.0@gmail.com>
2015-07-21 22:01:30 -0500

    render tags to create room

--- modules/rooms/models.py
+++ modules/rooms/models.py
@@ -31,7 +31,7 @@ class Tag(models.Model):
 	name = models.CharField(max_length=50)
 
 	def __unicode__(self):
-		return u"tag : {}".format(self.name)
+		return u"{}".format(self.name)
 
 
 class User_Room(models.Model):

53160f0a0303434519324c35dd8364c2af12f847
wils0n <wiljm0.0@gmail.com>
2015-07-22 16:47:09 -0500

    refactoring login in raiz

--- modules/perfil/urls.py
+++ modules/perfil/urls.py
@@ -6,7 +6,7 @@ from .views import LoginTemplateView, DashboardTemplateView
 urlpatterns = patterns('',
 
     url(r'^logout/$', 'modules.perfil.views.logout_view', name='auth_logout'),
-    url(r'^login/$', LoginTemplateView.as_view(), name='auth_login'),
+    url(r'^$', LoginTemplateView.as_view(), name='auth_login'),
     url(r'^dashboard/$', DashboardTemplateView.as_view(), name='perfil_dashboard'),
 )
 
